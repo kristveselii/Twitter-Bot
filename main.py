75985bb22ecbfa2c4da9ab96efacf37599e8b6d6
kristveselii <kristveseli04@gmail.com>
2025-08-15 22:36:23 -0400

    Small main setup

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import tweepy
-# from Tkinter import *
+from config_global import *
+from tkinter import *
 
-consumer_key = 'consumer_key'
+consumer_key = CONSUMER_KEY
+consumer_secret = CONSUMER_SECRET
+access_token = ACESS_TOKEN
+access_token_secret = ACESS_TOKEN_SECRET
+
