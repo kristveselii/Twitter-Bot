371af82126851beb930bc26f3d3a8e51d1482aba
kristveselii <kristveseli04@gmail.com>
2025-08-16 20:06:58 -0400

    Added Reply to Keyword

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tweepy
 from config import *
-from tkinter import *
 
 consumer_key = CONSUMER_KEY
 consumer_secret = CONSUMER_SECRET
@@ -8,7 +7,9 @@ access_token = ACESS_TOKEN
 access_token_secret = ACESS_TOKEN_SECRET
 
 def authenticate_twitter_app():
-    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key, consumer_secret, access_token, access_token_secret
+    )
     api = tweepy.API(auth)
     try:
         api.verify_credentials()
@@ -17,8 +18,8 @@ def authenticate_twitter_app():
     except Exception as e:
         print("Authentication failed:", e)
         return None
-    
-api = authenticate_twitter_app()
+
+#api = authenticate_twitter_app()
 
 # Follow everyone who follows you
 def follow_followers(api):
@@ -27,10 +28,33 @@ def follow_followers(api):
         return
     
     try:
-        for follower in tweepy.Cursor(api.followers).items():
+        for follower in tweepy.Cursor(api.get_followers).items():
             follower.follow()
             print(f"Followed {follower.screen_name}")
     except Exception as e:
         print("An error occurred while following followers:", e)
     
     print("Finished following all followers.")
+
+#follow_followers(api)
+
+def reply_to_keyword(api, keyword, reply_message):
+    if api is None:
+        print("API not authenticated. Cannot reply to tweets.")
+        return
+    
+    try:
+        for tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang="en").items(5):
+            try:
+                api.update_status(
+                    status=f"@{tweet.user.screen_name} {reply_message}",
+                    in_reply_to_status_id=tweet.id
+                )
+                print(f"Replied to {tweet.user.screen_name}")
+            except Exception as e:
+                print(f"Failed to reply to {tweet.user.screen_name}: {e}")
+    except Exception as e:
+        print("An error occurred while searching for tweets:", e)
+    
+    print("Finished replying to tweets.")
+
